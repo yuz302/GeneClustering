be9cd6809106e886eac72e1d34194bc880811e42
diana <dianazhou9501@yahoo.com>
2016-05-04 17:21:43 -0700

    std
    calculate and add std to the end of each row

--- get_data_table.py
+++ get_data_table.py
@@ -1,7 +1,8 @@
 import os
 import csv
+import numpy as np
 
-path = '/Users/diana/Desktop/cse199/9a7dd21e-e960-48b8-a734-ea5fce8b41bc/Expression-Genes/UNC__AgilentG4502A_07_3/Level_3'
+path = '/Users/diana/GitHub/GeneClustering/9a7dd21e-e960-48b8-a734-ea5fce8b41bc/Expression-Genes/UNC__AgilentG4502A_07_3/Level_3'
 
 with open('genes.csv', 'wb') as outfile:
     wr = csv.writer(outfile, delimiter=',',
@@ -11,6 +12,7 @@ with open('genes.csv', 'wb') as outfile:
     first_row = ['id']
     for i in range(len(os.listdir(path))-1):
         first_row.append('s'+str(i+1))
+    first_row.append('std')
     wr.writerow(first_row)
 
     ls = os.listdir(path)
@@ -20,7 +22,7 @@ with open('genes.csv', 'wb') as outfile:
     lss = [x[0:23] for x in ls]
     psn = [lss,first_row]
 
-    # get gene info from first file
+    # get gene info from the first file
     filename1 = ls.pop(0)
     columns = []
     gene_names = []
@@ -32,6 +34,7 @@ with open('genes.csv', 'wb') as outfile:
             gene_names.append(row.split()[0])
             column1.append(row.split()[1])
 
+    # add column to columns
     columns.append(gene_names)
     columns.append(column1)
 
@@ -44,11 +47,24 @@ with open('genes.csv', 'wb') as outfile:
 
             for row in infile.readlines():
                 one_file.append(row.split()[1])
-
+            # add this column to columns
             columns.append(one_file)
 
     table = zip(*columns)
     for row in table:
+        # get the std for each row, add to last column
+        last = row[1:]
+        dd = []
+        for i in last:
+            try:
+                float(i)
+            except:
+                continue
+            dd.append(float(i))
+
+        standd = np.std(dd)
+        sdd = (standd, )
+        row = row + sdd
         wr.writerow(row)
         # book.save("genes.xls")
 
